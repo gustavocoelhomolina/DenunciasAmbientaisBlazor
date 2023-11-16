d7dccd6246ec18e6eef2ba44120557382e0f43e7
Gustavo Coelho Molina <nextronferion@gmail.com>
2023-11-16 02:23:25 -0300

    Adicionar arquivos de projeto.

---
+++ DenunciasAmbientaisBlazor/Models/DenunciasModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DenunciasAmbientaisBlazor.Models
+{
+    public class DenunciasModel
+    {
+        public int Id { get; set; }
+        public string DescricaoDenuncia { get; set; }
+        public string LocalDenuncia { get; set; }
+
+        public TipoDenuncia Tipo { get; set; }
+        public DateTime DataDenuncia { get; set; }
+        public string? FotoDenuncia { get; set; }
+
+
+        public DateTime DataVerificacao { get; set; }
+        public bool? DenunciaVerificada { get; set; }
+        public bool? DenunciaEncaminhadaParaAutoridades { get; set; }
+        public string? QualAutoridadeFoiEncaminhada { get; set; }
+    }
+}
---
+++ DenunciasAmbientaisBlazor/Models/TipoDenuncia.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace DenunciasAmbientaisBlazor.Models
+{
+    public enum TipoDenuncia
+    {
+        [Description("Desmatamento")]
+        Desmatamento = 1,
+
+        [Description("Poluição do Ar")]
+        PoluicaoAr = 2,
+
+        [Description("Poluição da Água")]
+        PoluicaoAgua = 3,
+
+        [Description("Caça Ilegal")]
+        CacaIlegal = 4,
+
+        [Description("Tráfico de Animais")]
+        TraficoAnimais = 5,
+
+        [Description("Descarte Irregular de Resíduos")]
+        DescarteIrregular = 6,
+
+        [Description("Pesca Predatória")]
+        PescaPredatoria = 7,
+
+        [Description("Queimadas Ilegais")]
+        QueimadasIlegais = 8,
+
+        [Description("Potencial de Enchentes")]
+        PotencialEnchentes = 9,
+
+        [Description("Abuso de animais")]
+        AbusoAnimais = 10,
+
+        [Description("Outras denúncias (especifique)")]
+        OutrasDenuncias = 11,
+    }
+}
